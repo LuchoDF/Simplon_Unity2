3d0d6d7d6e26485e37680378e0f3e4453004bae0
luchodf <luchodellafelice@hotmail.com>
2024-06-27 16:20:21 +0200

    Modify script and scene2

--- My project/Assets/AgentManager.cs
+++ My project/Assets/AgentManager.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentManager : MonoBehaviour
-{
+{   
+    [SerializeField] private GameObject agent1;
+    [SerializeField] private GameObject agent2;
+    [SerializeField] private GameObject agent3;
+
+    public float changeSpeed = 10;
+    private static float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent1.transform.localScale = new Vector3 (2.0f, 0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rotate(agent1, changeSpeed = 5);
+
     }
 }
