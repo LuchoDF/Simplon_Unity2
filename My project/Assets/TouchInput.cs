942a3374b263580b807dfe07c75d8eaa661a3a83
luchodf <luchodellafelice@hotmail.com>
2024-07-04 15:56:21 +0200

    Modify touch script

---
+++ My project/Assets/TouchInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchInput : MonoBehaviour{
+    private Camera mainCamera;
+
+    void Start() {
+        mainCamera = Camera.main;
+    }
+
+    void Update() {
+
+        if (!Touchscreen.current.primaryTouch.press.IsPressed()) {
+            return.
+        }
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        Debug.Log("TOUCH POSITION: " + touchPosition);
+        Debug.Log("WORLD POSITION: " + worldPositionPosition);
+
+
+    }
+}
