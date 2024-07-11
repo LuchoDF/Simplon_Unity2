aeadd4be72a161076ef9a4eec4c7569aa821cc60
luchodf <luchodellafelice@hotmail.com>
2024-07-11 10:59:32 +0200

    Move ball

--- My project/Assets/TouchInput.cs
+++ My project/Assets/TouchInput.cs
@@ -5,9 +5,13 @@ using UnityEngine.InputSystem;
 
 public class TouchInput : MonoBehaviour{
     private Camera mainCamera;
+    private Rigidbody2D rb;
 
     void Start() {
         mainCamera = Camera.main;
+        // Récupère le Rigidbody2D de la balle
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     void Update() {
@@ -18,6 +22,8 @@ public class TouchInput : MonoBehaviour{
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        rb.MovePosition(new Vector2(worldPosition.x, worldPosition.y));
 
         Debug.Log("TOUCH POSITION: " + touchPosition);
         Debug.Log("WORLD POSITION: " + worldPosition);
