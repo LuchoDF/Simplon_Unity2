15519c1e6805cf9d89b99b5024b3ddacc0722fcf
luchodf <luchodellafelice@hotmail.com>
2024-07-11 14:24:01 +0200

    Add launch and detach functions

--- My project/Assets/TouchInput.cs
+++ My project/Assets/TouchInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,58 @@ using UnityEngine.InputSystem;
 public class TouchInput : MonoBehaviour{
     private Camera mainCamera;
     private Rigidbody2D rb;
+    public Rigidbody2D ballRigidbody;
+    public SpringJoint2D ballSpringJoint;
+    private bool isDragging = false;
+
+    public float detachDelay = 0.2f;
+    public float respawnDelay = 0.2f;
 
     void Start() {
         mainCamera = Camera.main;
-        // Récupère le Rigidbody2D de la balle
-        rb = GetComponent<Rigidbody2D>();
-
     }
 
     void Update() {
 
-        if (!Touchscreen.current.primaryTouch.press.IsPressed()) {
+        if (!Touchscreen.current.primaryTouch.press.IsPressed())
+        {   
+            if(isDragging)
+            {
+                LaunchBall();
+                // Invoke(nameof(SpawnBall),respawnDelay);
+                isDragging = false;
+            }
             return;
         }
 
+        isDragging = true;
+
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-
-        rb.MovePosition(new Vector2(worldPosition.x, worldPosition.y));
+        ballRigidbody.isKinematic = true;
+        ballRigidbody.position = worldPosition;
 
         Debug.Log("TOUCH POSITION: " + touchPosition);
         Debug.Log("WORLD POSITION: " + worldPosition);
 
+    }
 
+// private Void SpawnBall(){
+
+// }
+    private void LaunchBall(){
+        if (ballRigidbody != null){
+            ballRigidbody.isKinematic = false;
+            ballRigidbody = null;
+            Invoke(nameof(DetachBall), detachDelay);
+
+        }
+    }
+
+    private void DetachBall(){
+        if(ballSpringJoint != null){
+            ballSpringJoint.enabled = false;
+            ballSpringJoint = null;
+        }
     }
 }
